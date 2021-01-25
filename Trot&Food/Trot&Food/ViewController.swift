e6e09accfe038cf9d0bc89bea0ccf3d89188b03a
NicoBoe25 <55780311+NicoBoe25@users.noreply.github.com>
2021-01-25 10:50:39 +0100

    Initial commit

---
+++ Trot&Food/Trot&Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trot&Food
+//
+//  Created by boehrer nicolas on 25/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
