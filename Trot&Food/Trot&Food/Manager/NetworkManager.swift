2a1fd3d9c8c209b89b2bc6e2330d0e39fbe0de6a
Nicolas BOEHRER <nicolas.boehrer@edu.univ-fcomte.fr>
2021-02-08 09:22:18 +0100

    getUser in network manager (waiting for errors and indexing)

--- Trot&Food/Trot&Food/Manager/NetworkManager.swift
+++ Trot&Food/Trot&Food/Manager/NetworkManager.swift
@@ -15,14 +15,41 @@ class NetworkManager{
  
     private init(){}
     
-    func getUser(completed: @escaping (Result<[User], GFError>) -> Void) {
+    func getUser(completed: @escaping (Result<User, GFError>) -> Void) {
     
         let endpoint = baseURL + "/client/read.php?id=1"
         guard let url = URL(string: endpoint) else {
-            completed(.failure(.inavalidUsername))
+            completed(.failure(.inavalidUser))
             return
         }
-    
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+                
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+            
+        }
+        task.resume()
     }
     
     
